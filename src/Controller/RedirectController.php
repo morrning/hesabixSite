746ad08402db80635ec591aebcadbb755655d7a4
Babak Alizadeh <alizadeh.babak@gmail.com>
2025-02-14 08:18:39 +0000

    Merge branch 'master' of https://github.com/morrning/hesabixSite

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -11,12 +11,30 @@ class RedirectController extends AbstractController
     #[Route('/front/{url}', name: 'app_redirect_front')]
     public function app_redirect_front($url): Response
     {
-        return $this->redirectToRoute('app_page', ['url'=> $url]);
+        return $this->redirectToRoute('app_page', ['url' => $url]);
     }
 
     #[Route('/login', name: 'app_redirect_login')]
     public function app_redirect_login(): Response
     {
         return $this->redirect("https://app.hesabix.ir");
+    }
+
+    #[Route('/st/{params}', name: 'app_redirect_store', requirements: ['params' => '.+'])]
+    public function app_redirect_store($params): Response
+    {
+        return $this->redirect("https://api.hesabix.ir/st/" . $params);
+    }
+
+    #[Route('/st/{params}', name: 'app_redirect_sell', requirements: ['params' => '.+'])]
+    public function app_redirect_sell($params): Response
+    {
+        return $this->redirect("https://api.hesabix.ir/sl/" . $params);
+    }
+
+    #[Route('/p/rep/{params}', name: 'app_redirect_repservice', requirements: ['params' => '.+'])]
+    public function app_redirect_repservice($params): Response
+    {
+        return $this->redirect("https://api.hesabix.ir/p/rep/" . $params);
     }
 }
